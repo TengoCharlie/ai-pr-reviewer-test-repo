9cd5443390fd602fa071faf1c22325e1d14684fc
Harsh Tenguriya <32489875+TengoCharlie@users.noreply.github.com>
2023-10-11 10:49:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ module.exports = async function (context, req) {
     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
   const data = await dbInstance.Customers.findAll();
+  console.log(data);
   context.res = {
     // status: 200, /* Defaults to 200 */
     body: data,
